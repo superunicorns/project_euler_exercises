13a1ced4c9bace3f1ab9f7b080823a972c20a309
UnicornDad8 <cecibenitezca@gmail.com>
2026-08-05 15:44:59 -0300

    solve first project euler problem "multiples of 3 or 5".

---
+++ multiples_of_3_or_5.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+num = 1000
+def multiples_of_3_or_5(num):
+    sum = 0
+    for i in range(1, num):
+        if (i % 3 == 0) or (i % 5 == 0):
+            sum += i
+
+    print(sum)
+    return sum
+
+multiples_of_3_or_5(num)
